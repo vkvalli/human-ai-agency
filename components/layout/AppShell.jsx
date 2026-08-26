fb10231ae43da6be6037b41f59cc4664591f8165
Avni Jaglan <avnijaglan@Avnis-MacBook-Pro.local>
2026-08-26 13:56:21 -0700

    remove ocean theme

--- components/layout/AppShell.jsx
+++ components/layout/AppShell.jsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
-import OceanBackdrop from "./OceanBackdrop";
 import Topbar from "./Topbar";
 import PageContainer from "./PageContainer";
 
 export default function AppShell() {
   return (
-    <div className="relative min-h-screen overflow-x-hidden bg-[#04111b] text-slate-900">
-      <OceanBackdrop />
+    <div className="relative min-h-screen overflow-x-hidden bg-slate-100 text-slate-900">
       <div className="relative z-10">
         <Topbar />
         <PageContainer>
